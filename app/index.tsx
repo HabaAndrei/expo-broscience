7d2a52da5910521a02d2b61f45a8014f60ad4d27
Haba Andrei <habaconstantin45@gmail.com>
2025-05-30 21:15:18 +0300

    update index

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import { View, Text } from 'react-native';
 const index = () => {
   return (
-    <div>index</div>
+    <View>
+      <Text>
+        index
+      </Text>
+    </View>
   )
 }
 
