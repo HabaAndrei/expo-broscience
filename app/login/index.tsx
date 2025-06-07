36bbc86ecfeaf99ad4feb43315944845704aea1b
Haba Andrei <habaconstantin45@gmail.com>
2025-06-07 12:23:00 +0300

    dd auth form

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native';
+import AuthForm from '@/components/userDetails/AuthForm';
 
 export default function LoginIndex(){
 
   return (
     <View>
       <Text>login</Text>
+      <AuthForm/>
     </View>
   )
 }
