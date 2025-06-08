43d238d297c62730a730505447aa5b264efc04d8
Haba Andrei <habaconstantin45@gmail.com>
2025-06-08 14:56:55 +0300

    resolve identation

--- components/userDetails/Gender.tsx
+++ components/userDetails/Gender.tsx
@@ -6,17 +6,17 @@ export default function Gender(props){
   return (
     <View>
       <Text>Gender</Text>
-        <RadioGroup aria-labelledby="Select one item" name="form"
-          onValueChange={(newVal)=>{
-            props.dispatch({ type: 'setGender', payload: newVal })
-          }}
-        >
-          <YStack width={300} alignItems="center" space="$2">
-            {props.value.pages.Gender.values.map((value, index)=>{
-              return (
-                <RadioGroupItemWithLabel key={index} size="$5" value={index} label={value} />
-              )
-            })}
+      <RadioGroup aria-labelledby="Select one item" name="form"
+        onValueChange={(newVal)=>{
+          props.dispatch({ type: 'setGender', payload: newVal })
+        }}
+      >
+        <YStack width={300} alignItems="center" space="$2">
+          {props.value.pages.Gender.values.map((value, index)=>{
+            return (
+              <RadioGroupItemWithLabel key={index} size="$5" value={index} label={value} />
+            )
+          })}
         </YStack>
       </RadioGroup>
     </View>
