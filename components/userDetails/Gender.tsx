1f4e3ec415d6a5333131fd9b4325c30bf01e3529
Haba Andrei <habaconstantin45@gmail.com>
2025-06-08 14:49:45 +0300

    create Gnder component

---
+++ components/userDetails/Gender.tsx
@@ -0,0 +1,24 @@
+import { View, Text } from 'react-native';
+import { RadioGroup, YStack } from 'tamagui'
+import RadioGroupItemWithLabel from '@/components/RadioGroupItemWithLabel';
+
+export default function Gender(props){
+  return (
+    <View>
+      <Text>Gender</Text>
+        <RadioGroup aria-labelledby="Select one item" name="form"
+          onValueChange={(newVal)=>{
+            props.dispatch({ type: 'setGender', payload: newVal })
+          }}
+        >
+          <YStack width={300} alignItems="center" space="$2">
+            {props.value.pages.Gender.values.map((value, index)=>{
+              return (
+                <RadioGroupItemWithLabel key={index} size="$5" value={index} label={value} />
+              )
+            })}
+        </YStack>
+      </RadioGroup>
+    </View>
+  )
+}
