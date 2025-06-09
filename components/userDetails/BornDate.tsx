dbff20b1f3319000fb0804f7921b06aed71f1300
Haba Andrei <habaconstantin45@gmail.com>
2025-06-09 10:49:08 +0300

    edit BornDate styling

--- components/userDetails/BornDate.tsx
+++ components/userDetails/BornDate.tsx
@@ -1,13 +1,19 @@
 import { View, Text } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { useState } from 'react';
+import { H4 } from 'tamagui';
 
 export default function BornDate({date, setBornDate}: any){
   const [spinnerDate, setSpinnerDate] = useState<Date>(new Date(date));
   return (
-    <View>
-      <Text>BornDate</Text>
+    <View style={{alignItems: 'center'}} >
+      <H4>Select your born date</H4>
+
       <DateTimePicker
+        style={{margin: 30}}
+        textColor="black"
+        design="material"
+        maximumDate={new Date(2014, 0, 1)}
         mode="date"
         display="spinner"
         value={spinnerDate}
