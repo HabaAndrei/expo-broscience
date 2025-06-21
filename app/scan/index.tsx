a68d3e5189416104d1a651dc4e3b2897db4ddea0
Haba Andrei <habaconstantin45@gmail.com>
2025-06-21 16:57:18 +0300

    update ScanIndex component

--- app/scan/index.tsx
+++ app/scan/index.tsx
@@ -1,9 +1,35 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { Stack } from 'expo-router';
 import HomeButton from '@/components/Buttons/Home';
 import CameraUploader from '@/components/scan/CameraUploader';
+import { useState } from 'react';
+import { Button } from 'tamagui';
+import { Image } from "expo-image";
 
 export default function ScanIndex(){
+
+  const [uri, setUri] = useState<string | null>(null);
+
+  const renderPicture = () => {
+    return (
+      <View style={styles.imagePreviewContainer}>
+        <Image
+          source={{ uri }}
+          contentFit="contain"
+          style={styles.imagePreview}
+        />
+        <Button
+          size="$4"
+          theme="active"
+          marginTop="$4"
+          themeInverse
+        >
+          Analyze
+        </Button>
+      </View>
+    );
+  };
+
   return (
     <>
       <Stack.Screen
@@ -13,9 +39,24 @@ export default function ScanIndex(){
         }}
       />
       <View style={{flex: 1}} >
-        <Text>Scan index</Text>
-        <CameraUploader/>
+        <CameraUploader setUri={setUri} uri={uri}/>
+      </View>
+      <View>
+        {uri ? renderPicture() : null}
       </View>
     </>
   )
 }
+
+const styles = StyleSheet.create({
+  imagePreviewContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  imagePreview: {
+    width: 300,
+    height: 300,
+    borderRadius: 12,
+  },
+})
