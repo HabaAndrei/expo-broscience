fd1067c5b474f05e449726c03d74642625f24aa8
Haba Andrei <habaconstantin45@gmail.com>
2025-06-21 14:02:15 +0300

    scan route with header

--- app/scan/index.tsx
+++ app/scan/index.tsx
@@ -1,9 +1,16 @@
 import { View, Text } from 'react-native';
-
+import { Stack } from 'expo-router';
+import HomeButton from '@/components/Buttons/Home';
 
 export default function ScanIndex(){
   return (
     <View>
+      <Stack.Screen
+        options={{
+          headerTitle: "Scan food",
+          headerRight: () => <HomeButton/>
+        }}
+      />
       <Text>Scan index</Text>
     </View>
   )
