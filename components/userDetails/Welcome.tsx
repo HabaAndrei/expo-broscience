f9ab10a1af919ff0df32d2bce7f19abbfd9ca6f7
Haba Andrei <habaconstantin45@gmail.com>
2025-06-21 09:10:05 +0300

    add button at welcome

--- components/userDetails/Welcome.tsx
+++ components/userDetails/Welcome.tsx
@@ -1,9 +1,13 @@
 import { View, Text } from 'react-native';
+import { Button } from 'tamagui';
 
-export default function Welcome(){
+export default function Welcome(props: any){
   return (
     <View>
       <Text>Welcome</Text>
+      <Button
+        onPress={()=>props.handleChangeDispatch({ type: 'setCurrentPage', payload: "AuthForm" })}
+      > Login </Button>
     </View>
   )
 }
