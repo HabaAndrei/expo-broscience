cc24cc90a180d3eaa8a43e678c78bf73f5bdd03c
Haba Andrei <habaconstantin45@gmail.com>
2025-06-26 22:40:22 +0300

    create isBase64 function in diverse

--- helpers/diverse.ts
+++ helpers/diverse.ts
@@ -1,3 +1,13 @@
 // Simple email validation
 export const isValidEmail = (email: string) =>
   /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+// Verify if is base 64
+export function isBase64(str: string): string|boolean {
+  if (typeof str !== "string") {
+    return false;
+  }
+  str = str.replace('data:image/jpeg;base64,', '')
+  const base64Regex = /^(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)?$/;
+  return base64Regex.test(str);
+}
