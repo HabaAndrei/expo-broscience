3d926d23a076b384198f86973a28b9de72fe216b
Haba Andrei <habaconstantin45@gmail.com>
2025-07-06 19:51:50 +0300

    update index scan images

--- app/(tabs)/scan/index.tsx
+++ app/(tabs)/scan/index.tsx
@@ -1,4 +1,4 @@
-import { ScrollView } from 'react-native';
+import { ScrollView, SafeAreaView, StyleSheet } from 'react-native';
 import { Stack } from 'expo-router';
 import HomeButton from '@/components/Buttons/Home';
 import CameraUploader from '@/components/Scan/CameraUploader';
@@ -10,42 +10,41 @@ import CardFoodImage from '@/components/Scan/CardFoodImage';
 import TotalsFoodAnalysis from '@/components/Scan/TotalsFoodAnalysis';
 import IngredientsFoodAnalysis from '@/components/Scan/IngredientsFoodAnalysis';
 
-export default function ScanIndex(){
+export default function ScanIndex() {
 
   const [uri, setUri] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
-  const [analysisError, setAnalysisError] = useState({isError: false, message: ''});
+  const [analysisError, setAnalysisError] = useState({ isError: false, message: '' });
   const [analysis, setAnalysis] = useState({});
 
-  async function analyzeImage(){
+  async function analyzeImage() {
     if (!uri || isLoading) return;
     let image = await base64Image(uri);
     try {
       if (analysisError.isError) {
-        setAnalysisError({isError: false, message: ''});
+        setAnalysisError({ isError: false, message: '' });
       }
       setIsLoading(true);
       const resultAnalyses = await axios.post(EnvConfig.get('serverAddress') + '/analyzeImage',
-        {image}, { 'headers': { 'Content-Type': 'application/json' }}
+        { image }, { headers: { 'Content-Type': 'application/json' } }
       );
       const result = resultAnalyses.data;
       if (!result?.is_resolved) {
-        setAnalysisError({isError: true, message: 'Try again. The analyses could not be resolved.'})
+        setAnalysisError({ isError: true, message: 'Try again. The analyses could not be resolved.' });
       } else if (!result?.data?.is_food) {
-        setAnalysisError({isError: true, message: 'Try again with a food image this time.'})
+        setAnalysisError({ isError: true, message: 'Try again with a food image this time.' });
       }
       if (result?.is_resolved && result?.data?.is_food) {
         setAnalysis(result.data);
       }
       setIsLoading(false);
 
-    } catch(err) {
+    } catch (err) {
       console.log(err);
-      setAnalysisError({isError: true, message: 'We had a system error. Please try again.'})
+      setAnalysisError({ isError: true, message: 'We had a system error. Please try again.' });
       setIsLoading(false);
     }
   }
-
 
   return (
     <>
@@ -56,26 +55,37 @@ export default function ScanIndex(){
         }}
       />
 
-      <ScrollView style={{ flex: 1 }}>
-        <CameraUploader
-          setUri={setUri}
-          uri={uri}
-        />
-        <CardFoodImage
-          uri={uri}
-          isLoading={isLoading}
-          analyzeImage={analyzeImage}
-          analysisError={analysisError}
-        />
-        <TotalsFoodAnalysis
-          analysis={analysis}
-          setAnalysis={setAnalysis}
-        />
-        <IngredientsFoodAnalysis
-          analysis={analysis}
-          setAnalysis={setAnalysis}
-        />
-      </ScrollView>
+      <SafeAreaView style={styles.container}>
+        <ScrollView contentContainerStyle={styles.scrollView}>
+          <CameraUploader
+            setUri={setUri}
+            uri={uri}
+          />
+          <CardFoodImage
+            uri={uri}
+            isLoading={isLoading}
+            analyzeImage={analyzeImage}
+            analysisError={analysisError}
+          />
+          <TotalsFoodAnalysis
+            analysis={analysis}
+            setAnalysis={setAnalysis}
+          />
+          <IngredientsFoodAnalysis
+            analysis={analysis}
+            setAnalysis={setAnalysis}
+          />
+        </ScrollView>
+      </SafeAreaView>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollView: {
+    paddingBottom: 50,
+  }
+});
