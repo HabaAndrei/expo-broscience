0499b8fd7efc4ef918698a0a2ee135a5f8bcbd0b
Haba Andrei <habaconstantin45@gmail.com>
2025-07-25 08:15:19 +0300

    update SearchResults component

--- components/SearchFood/SearchResults.tsx
+++ components/SearchFood/SearchResults.tsx
@@ -1,9 +1,9 @@
 import { Button, Text, YStack } from "tamagui"
 
-export default function SearchResults(props:any){
+export default function SearchResults(props: any) {
   return (
     <YStack
-      width='80%'
+      width="80%"
       position="absolute"
       top={48}
       borderRadius={10}
@@ -12,26 +12,37 @@ export default function SearchResults(props:any){
       elevation={5}
       maxHeight={200}
       overflow="scroll"
+      backgroundColor="white"
       shadowColor="#000"
       shadowOpacity={0.1}
       shadowRadius={10}
       shadowOffset={{ width: 0, height: 4 }}
       zIndex={1000}
-      backgroundColor="white"
-      style={{ alignSelf: 'center' }}
+      alignSelf="center"
     >
       {props.options.map((option: any, idx: number) => (
         <Button
-          key={option?.name}
-          onPress={()=>props.func(option)}
+          key={idx}
+          onPress={() => props.func(option)}
           size="$4"
+          justifyContent="center"
+          alignItems="center"
+          backgroundColor="transparent"
           borderBottomWidth={idx === props.options.length - 1 ? 0 : 1}
           borderBottomColor="#eee"
-          backgroundColor="transparent"
+          px="$3"
+          py="$2"
         >
-          <Text fontSize={15} color="#333">
-            {option.name}
-          </Text>
+          <YStack alignItems="center">
+            <Text fontSize={15} color="#333" fontWeight="600">
+              {option.food_name}
+            </Text>
+            {option.brand_name && (
+              <Text fontSize={13} color="#888">
+                {option.brand_name}
+              </Text>
+            )}
+          </YStack>
         </Button>
       ))}
     </YStack>
