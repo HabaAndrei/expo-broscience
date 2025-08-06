06145b1205c4797d9f0165bb9be9c6e33fc30e29
Haba Andrei <habaconstantin45@gmail.com>
2025-08-06 22:47:07 +0300

    create formatDateToYMD function

--- helpers/diverse.tsx
+++ helpers/diverse.tsx
@@ -261,3 +261,10 @@ export const nutrientsIcons: any = {
   carbohydrate: <Cake size="$1.5" color="#C58A47" />,
   fat: <Droplet size="$1.5" color="#4D9DE0" />
 }
+
+export function formatDateToYMD(date: Date): string {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // months are 0-indexated
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+}
