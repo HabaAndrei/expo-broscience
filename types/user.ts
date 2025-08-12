3bdf09b51f6f359d2fac9cfe8909f03fab4839f2
Haba Andrei <habaconstantin45@gmail.com>
2025-08-12 10:20:59 +0300

    create user universal type

---
+++ types/user.ts
@@ -0,0 +1,14 @@
+export type UserDetails = {
+  bornDate: string;
+  createdAt: any;
+  email: string;
+  emailVerified: boolean;
+  firstName: string | undefined;
+  gender: string;
+  goal: string;
+  height: number | string;
+  secondName: string;
+  uid: string;
+  weight: number | string;
+  workouts: number | string;
+};
