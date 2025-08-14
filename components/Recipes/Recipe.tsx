4c2aeeeb0e75d5cbf60a81231b123f47ab025bd4
Haba Andrei <habaconstantin45@gmail.com>
2025-08-14 09:44:32 +0300

    create RecipeServingLabel

--- components/Recipes/Recipe.tsx
+++ components/Recipes/Recipe.tsx
@@ -1,10 +1,22 @@
 import { View, Text } from 'tamagui';
 import { Recipe as RecipeType } from '@/types/food';
+import RecipeServingLabel from '@/components/Recipes/RecipeServingLabel';
 
 export default function Recipe({recipe}:{recipe: RecipeType }){
+
+  const { cooking_time_min, directions, grams_per_portion, ingredients,
+    number_of_servings, preparation_time_min, rating, recipe_description, recipe_images,
+    recipe_name, serving_sizes
+  } = recipe ?? {};
+
+  const { ingredient } = ingredients ?? {}; // ingredient is an array
+  const { direction } = directions ?? {}; // direction is an array
+  const { serving } = serving_sizes ?? {}
+
+
   return (
     <View>
-      <Text>okok</Text>
+      <RecipeServingLabel serving={serving} grams_per_portion={grams_per_portion} />
     </View>
   )
 }
