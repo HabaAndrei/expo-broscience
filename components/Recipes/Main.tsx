15b5e1a14e3e0b9d9d21dc1261e2a46f19a7d4a1
Haba Andrei <habaconstantin45@gmail.com>
2025-08-16 10:30:56 +0300

    add styling for recipes

--- components/Recipes/Main.tsx
+++ components/Recipes/Main.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import { View } from "tamagui";
+import { View, XStack } from "tamagui";
 import QueryTable from '@/components/Recipes/QueryTable';
 import { Recipe } from '@/types/food';
 import Recipes from '@/components/Recipes/Recipes';
@@ -7,7 +7,6 @@ import axios from 'axios';
 import { EnvConfig } from '@/providers/EnvConfig';
 import { ArrowRightCircle, ArrowLeftCircle } from '@tamagui/lucide-icons'
 import { Pressable } from "react-native";
-
 
 type NutrientFilter = {
   minValue: string;
@@ -21,9 +20,7 @@ export type Filter = {
   protein: NutrientFilter;
 };
 
-
 export default function Main() {
-
   const [recipes, setRecipes] = useState<Recipe[] | []>([]);
   const [filter, setFilter] = useState<Filter>({
     calories: { minValue: '', maxValue: '' },
@@ -32,25 +29,32 @@ export default function Main() {
     protein: { minValue: '', maxValue: '' }
   });
   const [searchText, setSearchText] = useState("");
-  const pagination = useRef({limit: 10, offset: 0});
+  const pagination = useRef({ limit: 10, offset: 0 });
 
-  useEffect(()=>{
+  useEffect(() => {
     pagination.current.offset = 0;
     getRecipes();
   }, [filter, searchText]);
 
-  async function getRecipes(){
+  async function getRecipes() {
     try {
-      const resultSearch = await axios.get(EnvConfig.get('serverAddress') + "/search-recipe", {
-        params: {input: searchText, filter: JSON.stringify(filter), pagination: JSON.stringify(pagination.current)}
-      });
+      const resultSearch = await axios.get(
+        EnvConfig.get('serverAddress') + "/search-recipe",
+        {
+          params: {
+            input: searchText,
+            filter: JSON.stringify(filter),
+            pagination: JSON.stringify(pagination.current)
+          }
+        }
+      );
       if (!resultSearch?.data?.is_resolved) {
         console.log("the search is not completed");
         return;
       }
       const recipes = resultSearch?.data?.data;
-      setRecipes(recipes ?? [])
-    }catch(err){
+      setRecipes(recipes ?? []);
+    } catch (err) {
       console.log("the search is not completed", err);
     }
   }
@@ -58,23 +62,24 @@ export default function Main() {
   const isDisabledLeft = pagination.current.offset < 1;
   const isDisabledRight = recipes?.length < pagination.current.limit;
 
-  function changePage(direction: 'left' | 'right'){
-    if (direction == 'left'){
+  function changePage(direction: 'left' | 'right') {
+    if (direction == 'left') {
       const newVal = pagination.current.offset - pagination.current.limit;
       if (newVal < 0) {
         pagination.current.offset = 0;
         return;
       }
       pagination.current.offset = newVal;
-    }else if (direction == 'right'){
-      if (recipes?.length < pagination.current.limit ) return;
-      pagination.current.offset = pagination.current.offset + pagination.current.limit;
+    } else if (direction == 'right') {
+      if (recipes?.length < pagination.current.limit) return;
+      pagination.current.offset =
+        pagination.current.offset + pagination.current.limit;
     }
     getRecipes();
   }
 
   return (
-    <View style={{paddingBottom: 100}} >
+    <View style={{ paddingBottom: 100 }}>
       <QueryTable
         setFilter={setFilter}
         filter={filter}
@@ -82,24 +87,32 @@ export default function Main() {
         searchText={searchText}
       />
 
-      {recipes.length ?
-        <Recipes recipes={recipes} /> : null
-      }
+      {recipes.length ? <Recipes recipes={recipes} /> : null}
 
-        <View>
-          <Pressable
-            disabled={isDisabledLeft}
-            onPress={()=>changePage('left')}
-          >
-            <ArrowLeftCircle></ArrowLeftCircle>
-          </Pressable>
-          <Pressable
-            disabled={isDisabledRight}
-            onPress={()=>changePage('right')}
-          >
-            <ArrowRightCircle></ArrowRightCircle>
-          </Pressable>
-        </View>
+      {/* Pagination Arrows */}
+      <XStack  style={{justifyContent: "center", alignItems: "center"}} gap="$4" mt="$6">
+        <Pressable
+          disabled={isDisabledLeft}
+          onPress={() => changePage('left')}
+        >
+          <ArrowLeftCircle
+            size={40}
+            color={isDisabledLeft ? "#ccc" : "#0f0f0f"}
+            opacity={isDisabledLeft ? 0.5 : 1}
+          />
+        </Pressable>
+
+        <Pressable
+          disabled={isDisabledRight}
+          onPress={() => changePage('right')}
+        >
+          <ArrowRightCircle
+            size={40}
+            color={isDisabledRight ? "#ccc" : "#0f0f0f"}
+            opacity={isDisabledRight ? 0.5 : 1}
+          />
+        </Pressable>
+      </XStack>
     </View>
   );
 }
