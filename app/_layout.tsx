b0f53d629ec477c144f455f1daff0c3f98159b39
Haba Andrei <habaconstantin45@gmail.com>
2025-09-02 14:06:45 +0300

    updat tamagui config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -55,7 +55,6 @@ export default function RootLayout() {
                 <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
                   <Stack>
                     <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                    <Stack.Screen name="test" options={{ headerShown: false }} />
                     <Stack.Screen name="+not-found" options={{ headerShown: false }} />
                     <Stack.Screen name="login" options={{ headerShown: false }} />
                   </Stack>
