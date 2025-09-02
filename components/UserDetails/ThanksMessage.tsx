3655bf31d4c6875b09726416f79e617f194e7f42
Haba Andrei <habaconstantin45@gmail.com>
2025-09-02 18:46:41 +0300

    Thanks Message component

--- components/UserDetails/ThanksMessage.tsx
+++ components/UserDetails/ThanksMessage.tsx
@@ -1,9 +1,34 @@
-import { View, Text } from 'react-native';
+import { YStack, H2, Paragraph, Image } from 'tamagui'
 
-export default function ThanksMessage(props: any){
+export default function ThanksMessage() {
   return (
-    <View>
-      <Text>ThanksMessage</Text>
-    </View>
+    <YStack
+      flex={1}
+      style={{justifyContent: "center", alignItems: "center"}}
+      p="$6"
+      space="$4"
+    >
+      <Image
+        source={require('@/assets/images/icon.png')}
+        width={80}
+        height={80}
+        borderRadius="$6"
+        mb="$3"
+      />
+
+      <H2 fontWeight="700" color="$color12" style={{alignSelf: "center"}}>
+        Thank You for Choosing Bro Science
+      </H2>
+
+      <Paragraph size="$5" color="$color10" style={{alignSelf: "center"}}>
+        We’re excited to support your journey toward healthier eating and better living.
+      </Paragraph>
+
+      <Paragraph size="$4" color="$color9" style={{alignSelf: "center"}}>
+        Your privacy is our top priority. We keep your data safe and secure, and we are
+        committed to delivering the highest quality service to help you reach your goals.
+      </Paragraph>
+
+    </YStack>
   )
 }
